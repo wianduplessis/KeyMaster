df7c7b44bb268c3539753ee2fa1c866d194f776a
Wian du Plessis <wianduppie2@gmail.com>
2024-02-25 21:38:30 +0200

    added algorithm to test for input
    entered words will now appear green

--- app.js
+++ app.js
@@ -6,28 +6,73 @@ const words = [
     'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me'
   ];
 
-const paragraphDisplayElement = document.getElementById('paragraph');
-const inputElement = document.getElementById('textbox');
+  const paragraphSize = 10;
 
+// Create a random paragraph of words by selecting random words from array
 function createParagraph(){
-    paragraphSize = 150;
-    paragraph = "";
+    
+    let paragraph = "";
 
     for(let i = 0; i < paragraphSize; i++ ){
         paragraph += words[Math.floor(Math.random()*words.length)];
 
-        if(i != paragraphSize-1){
+        if(i !== paragraphSize - 1)
             paragraph += " ";
-        }
     }
-    
     return paragraph;
 }
 
-async function getParagraph(){
+const paragraphSection = document.getElementById('paragraph');
+let userInput = document.getElementById('textbox');
+let counter = 0;
+
+
+async function renderParagraph(){
+
+    let spanId = 0;
+
     const paragraph = await createParagraph();
-    paragraphDisplayElement.innerText = paragraph;
-    inputElement.value = null;
+    paragraphSection.innerHTML = "";
+
+    paragraph.split(" ").forEach(word => {
+
+        const wordSpan = document.createElement('span');
+        wordSpan.id = "span" + spanId;
+        spanId++;
+        wordSpan.innerText= word + " ";
+        paragraphSection.appendChild(wordSpan)
+
+    });;
+
+    counter = 0;
+    userInput.value = null;
 }
 
-getParagraph();
+
+userInput.addEventListener("keydown", e => {
+  
+    wordArray = paragraphSection.querySelectorAll('span');
+
+    if(e.key === ' ' || e.code == "Space"){
+
+        if(userInput.value === wordArray[paragraphSize-1].innerText){
+            e.preventDefault()
+            console.log("DONE");
+            renderParagraph();
+        }
+
+        if(userInput.value + " " === wordArray[counter].innerText){
+            e.preventDefault();
+            console.log("ree");
+            userInput.value = null;
+            paragraphSection.querySelector("#span" + counter).classList = "correct";
+            counter++;
+
+            
+        }
+    }
+})
+
+
+
+renderParagraph();
